300f5820d93560026df34509f54b61b18eab9663
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-01-28 20:12:27 -0500

    v1

---
+++ activity1.py
@@ -0,0 +1,20 @@
+x = 5
+if (type(x) is int):
+    print("true")
+    
+else:
+    print("false")
+    
+    
+x = 5.5
+if (type(x) is not float):
+    print("true")
+    
+else:
+    print("false")
+    
+x = 20
+y = 20
+
+if (x is y):
+    print("x and y have the same identity")
